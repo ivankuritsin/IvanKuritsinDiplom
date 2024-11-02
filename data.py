65b6f6f1452becad22da7c801281bf8a10f01cd7
ivankuritsin <ivankuritsin86@gmail.com>
2024-11-02 08:59:17 +0300

    IvanDiplom

---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Uchiha",
+    "lastName": "Naruto",
+    "address": "Konoha, 143 apt.",
+    "metroStation": 1,
+    "phone": "+7 800 355 35 35",
+    "rentTime": 5,
+    "deliveryDate": "2024-10-30",
+    "comment": "Saske, come back to Konoha",
+    "color": [
+        "BLACK"
+    ]
+}
